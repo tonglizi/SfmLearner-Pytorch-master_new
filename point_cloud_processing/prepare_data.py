82417bbe56fd14ee02474715f74ea84890e7d4a0
tonglizi <931217336@qq.com>
2020-07-09 07:12:48 +0800

    Small fixes

--- point_cloud_processing/prepare_data.py
+++ point_cloud_processing/prepare_data.py
@@ -14,12 +14,3 @@ def loadData(rootdir):
             p=pcl.load(file)
             pointclouds.append(p)
     return pointclouds
-
-def
-p = pcl.load("C/table_scene_lms400.pcd")
-
-a = np.asarray(p)       # NumPy view on the cloud
-a[:] = 0                # fill with zeros
-print(p[3])             # prints (0.0, 0.0, 0.0)
-a[:, 0] = 1             # set x coordinates to 1
-print(p[3])             # prints (1.0, 0.0, 0.0)
