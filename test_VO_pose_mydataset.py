c0dd3eeb84fda4182234ea1005aaeb26274bb335
tonglizi <931217336@qq.com>
2021-03-09 21:59:53 +0800

    Updates

--- test_VO_pose_mydataset.py
+++ test_VO_pose_mydataset.py
@@ -16,7 +16,7 @@ from path import Path
 import argparse
 from tqdm import tqdm
 
-from models import PoseExpNet,DispNetS
+from models import PoseExpNet, DispNetS
 from inverse_warp import pose_vec2mat
 
 import argparse
@@ -29,6 +29,7 @@ np.set_printoptions(precision=4)
 from matplotlib.animation import FFMpegWriter
 
 from tqdm import tqdm
+from sympy import *
 
 # from minisam import *
 # how to install minisam: https://minisam.readthedocs.io/install.html
@@ -43,33 +44,27 @@ parser = argparse.ArgumentParser(
     description='Script for PoseNet testing with corresponding groundTruth from KITTI Odometry',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("pretrained_posenet", type=str, help="pretrained PoseNet path")
-parser.add_argument("--pretrained-dispnet", required=True, type=str, help="pretrained DispNet path")
+parser.add_argument("--pretrained-dispnet", type=str, help="pretrained DispNet path")
 parser.add_argument("--img-height", default=341, type=int, help="Image height")
 parser.add_argument("--img-width", default=427, type=int, help="Image width")
 parser.add_argument("--no-resize", action='store_true', help="no resizing is done")
 parser.add_argument("--min-depth", default=1e-3)
 parser.add_argument("--max-depth", default=80)
-
 parser.add_argument("--dataset-dir", default='.', type=str, help="Dataset directory")
 parser.add_argument("--sequences", default='09', type=str, nargs='*', help="sequences to test")
-parser.add_argument("--output-dir", default=None, type=str,
+parser.add_argument("--output-dir", default='./result/', type=str,
                     help="Output directory for saving predictions in a big 3D numpy file")
 parser.add_argument("--img-exts", default=['png', 'jpg', 'bmp'], nargs='*', type=str, help="images extensions to glob")
 parser.add_argument("--rotation-mode", default='euler', choices=['euler', 'quat'], type=str)
-
 parser.add_argument('--num_icp_points', type=int, default=5000)  # 5000 is enough for real time
-
 parser.add_argument('--num_rings', type=int, default=20)  # same as the original paper
 parser.add_argument('--num_sectors', type=int, default=60)  # same as the original paper
 parser.add_argument('--num_candidates', type=int, default=10)  # must be int
-parser.add_argument('--try_gap_loop_detection', type=int, default=10)  # same as the original paper
-
+parser.add_argument('--try_pose_graph_optimization', type=int, default=30)  # same as the original paper
 parser.add_argument('--loop_threshold', type=float,
                     default=0.11)  # 0.11 is usually safe (for avoiding false loop closure)
-
-
 parser.add_argument('--save_gap', type=int, default=300)
-parser.add_argument('--isDynamic', type=bool, default=False,help="Only for dynamic scene to test photo mask")
+parser.add_argument('--isDynamic', type=bool, default=False, help="Only for dynamic scene to test photo mask")
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
@@ -88,11 +83,12 @@ def main():
     pose_net.load_state_dict(weights['state_dict'], strict=False)
 
     # *************************可删除*********************************
-    # 为了进行Mask评估，这里需要引入disp net
-    disp_net = DispNetS().to(device)
-    weights = torch.load(args.pretrained_dispnet)
-    disp_net.load_state_dict(weights['state_dict'])
-    disp_net.eval()
+    # 为了进行动态场景下的Mask评估，这里需要引入disp net
+    if args.isDynamic:
+        disp_net = DispNetS().to(device)
+        weights = torch.load(args.pretrained_dispnet)
+        disp_net.load_state_dict(weights['state_dict'])
+        disp_net.eval()
 
     # normalize = custom_transforms.Normalize(mean=[0.5, 0.5, 0.5],
     #                                         std=[0.5, 0.5, 0.5])
@@ -121,21 +117,22 @@ def main():
     print('{} snippets to test'.format(len(framework)))
     errors = np.zeros((len(framework), 2), np.float32)
     optimized_errors = np.zeros((len(framework), 2), np.float32)
-    if args.output_dir is not None:
-        output_dir = Path(args.output_dir)
-        output_dir.makedirs_p()
-        predictions_array = np.zeros((len(framework), seq_length, 3, 4))
-        abs_VO_poses = np.zeros((len(framework), 12))
 
+    '''输出到文件夹中的数据'''
+    predictions_array = np.zeros((len(framework), seq_length, 3, 4))
+    abs_VO_poses = np.zeros((len(framework), 12))
+    processing_time = np.zeros(len(framework), 1)
+    rel_VO_poses = np.zeros(len(framework), 12)
+    '''循环中的变量'''
     abs_VO_pose = np.identity(4)[:3, :]
 
     # L和C的转换矩阵,对齐输入位姿到雷达坐标系
     Transform_matrix_L2C = np.identity(4)
-    Transform_matrix_L2C[:3, :3] = np.array([[-1.51482698e-02, -9.99886648e-01,  5.36310553e-03],
+    Transform_matrix_L2C[:3, :3] = np.array([[-1.51482698e-02, -9.99886648e-01, 5.36310553e-03],
                                              [-4.65337018e-03, -5.36307196e-03, -9.99969412e-01],
                                              [9.99870070e-01, -1.56647995e-02, -4.48880010e-03]])
     Transform_matrix_L2C[:3, -1:] = np.array([4.29029924e-03, -6.08539196e-02, -9.20346161e-02]).reshape(3, 1)
-
+    Transform_matrix_L2C = GramSchmidtHelper(Transform_matrix_L2C)
     Transform_matrix_C2L = np.linalg.inv(Transform_matrix_L2C)
 
     # *************可视化准备***********************
@@ -145,7 +142,7 @@ def main():
     PGM.addPriorFactor()
 
     # Result saver
-    save_dir = "result/" + args.sequences[0]
+    save_dir = args.output_dir + "mydataset/" + args.sequences[0]
     if not os.path.exists(save_dir): os.makedirs(save_dir)
     ResultSaver = PoseGraphResultSaver(init_pose=PGM.curr_se3,
                                        save_gap=args.save_gap,
@@ -190,10 +187,9 @@ def main():
                 else:
                     ref_imgs.append(img)
 
-            timeCostVO = 0
             startTimeVO = time.time()
             _, poses = pose_net(tgt_img, ref_imgs)
-            timeCostVO = time.time() - startTimeVO
+            processing_time[j] = time.time() - startTimeVO
 
             # **************************可删除********************************
             if args.isDynamic:
@@ -224,7 +220,7 @@ def main():
                     'zeros',
                     isTrain=False)
 
-                im_path = args.output_dir + '/result/' + args.sequences[0] + '/seq_{}/'.format(j)
+                im_path = save_dir + '/seq_{}/'.format(j)
                 if not os.path.exists(im_path):
                     os.makedirs(im_path)
                 # save tgt_img
@@ -241,161 +237,64 @@ def main():
                     img = Image.fromarray(np.uint8(warped_img)).convert('RGB')
                     # img.show()
                     if args.output_dir is not None:
-                        img.save(im_path+'src_{}.jpg'.format(i))
+                        img.save(im_path + 'src_{}.jpg'.format(i))
 
                 for i in range(len(weighted_masks[0])):
                     weighted_mask = weighted_masks[0][i].cpu().clone().numpy() * 255
                     img = Image.fromarray(weighted_mask)
-                    img=img.convert('L')
+                    img = img.convert('L')
                     # img.show()
                     if args.output_dir is not None:
-                        img.save(im_path+'photomask_{}.jpg'.format(i))
+                        img.save(im_path + 'photomask_{}.jpg'.format(i))
             # ***************************************************************
+
             poses = poses.cpu()[0]
             poses = torch.cat([poses[:len(imgs) // 2], torch.zeros(1, 6).float(), poses[len(imgs) // 2:]])
 
             inv_transform_matrices = pose_vec2mat(poses, rotation_mode=args.rotation_mode).numpy().astype(np.float64)
-
             rot_matrices = np.linalg.inv(inv_transform_matrices[:, :, :3])
             tr_vectors = -rot_matrices @ inv_transform_matrices[:, :, -1:]
-
             transform_matrices = np.concatenate([rot_matrices, tr_vectors], axis=-1)
-            print('**********DeepVO result: time_cost {:.3} s'.format(timeCostVO / (len(imgs) - 1)))
-            # print(transform_matrices)
             # 将对[0 1 2]中间1的转换矩阵变成对0的位姿转换
             first_inv_transform = inv_transform_matrices[0]
             final_poses = first_inv_transform[:, :3] @ transform_matrices
             final_poses[:, :, -1:] += first_inv_transform[:, -1:]
-            # print('first')
-            # print(first_inv_transform)
-            print('poses')
-            print(final_poses)
-
-            # cur_VO_pose取final poses的第2项，则是取T10,T21,T32。。。
-            cur_VO_pose = np.identity(4)
-            cur_VO_pose[:3, :] = final_poses[1]
-            # 引入尺度因子对单目VO输出的位姿进行修正 TODO 尺度因子如何确定
+            # rel_VO_pose取final poses的第2项，则是取T10,T21,T32。。。
+            rel_VO_pose = np.identity(4)
+            rel_VO_pose[:3, :] = final_poses[1]
+            # 引入尺度因子对单目VO输出的位姿进行修正,并进行坐标系对齐到雷达坐标系
             scale_factor = 7
-            cur_VO_pose[:3, -1:] = cur_VO_pose[:3, -1:] * scale_factor
-            print("对齐前的帧间位姿")
-            print(cur_VO_pose)
+            rel_VO_pose[:3, -1:] = rel_VO_pose[:3, -1:] * scale_factor
+            rel_VO_pose = Transform_matrix_C2L @ rel_VO_pose @ np.linalg.inv(Transform_matrix_C2L)
+            rel_VO_pose = GramSchmidtHelper(rel_VO_pose)
 
-            cur_VO_pose = Transform_matrix_C2L @ cur_VO_pose @ np.linalg.inv(Transform_matrix_C2L)
-            print("对齐到雷达坐标系帧间位姿")
-            print(cur_VO_pose)
-
-            PGM.curr_node_idx = j  # make start with 0
-            if (PGM.curr_node_idx == 0):
-                PGM.prev_node_idx = PGM.curr_node_idx
-                continue
-            # update the current (moved) pose
-            PGM.curr_se3 = np.matmul(PGM.curr_se3, cur_VO_pose)
-
-            # add the odometry factor to the graph
-            # PGM.addOdometryFactor(cur_VO_pose)
-
-            # renewal the prev information
+            PGM.curr_node_idx = j + 1
+            PGM.curr_se3 = np.matmul(PGM.curr_se3, rel_VO_pose)
+            PGM.addOdometryFactor(rel_VO_pose)
             PGM.prev_node_idx = PGM.curr_node_idx
 
             # loop detection and optimize the graph
-            if (PGM.curr_node_idx > 1 and PGM.curr_node_idx % args.try_gap_loop_detection == 0):
-                # 1/ loop detection
-                loop_idx, loop_dist, yaw_diff_deg = SCM.detectLoop()
-                if (loop_idx == None):  # NOT FOUND
-                    pass
-                # else:
-                #     print("Loop event detected: ", PGM.curr_node_idx, loop_idx, loop_dist)
-                #     # 2-1/ add the loop factor
-                #     loop_scan_down_pts = SCM.getPtcloud(loop_idx)
-                #     loop_transform, _, _ = ICP.icp(curr_scan_down_pts, loop_scan_down_pts,
-                #                                    init_pose=yawdeg2se3(yaw_diff_deg), max_iterations=20)
-                #     PGM.addLoopFactor(loop_transform, loop_idx)
-                #
-                #     # 2-2/ graph optimization
-                #     PGM.optimizePoseGraph()
-                #
-                #     # 2-2/ save optimized poses
-                #     ResultSaver.saveOptimizedPoseGraphResult(PGM.curr_node_idx, PGM.graph_optimized)
+            if (PGM.curr_node_idx > 1 and PGM.curr_node_idx % args.try_pose_graph_optimization == 0):
+                # 2-1/ graph optimization
+                PGM.optimizePoseGraph()
+                # 2-2/ save optimized poses
+                ResultSaver.saveOptimizedPoseGraphResult(PGM.curr_node_idx, PGM.graph_optimized)
 
-            # save the ICP odometry pose result (no loop closure)
             ResultSaver.saveUnoptimizedPoseGraphResult(PGM.curr_se3, PGM.curr_node_idx)
             if (j % num_frames_to_skip_to_show == 0):
                 ResultSaver.vizCurrentTrajectory(fig_idx=fig_idx)
                 writer.grab_frame()
 
             # 绝对位姿
-            abs_VO_pose[:, :3] = cur_VO_pose[:3, :3] @ abs_VO_pose[:, :3]
-            abs_VO_pose[:, -1:] += cur_VO_pose[:3, -1:]
+            abs_VO_pose[:, :3] = rel_VO_pose[:3, :3] @ abs_VO_pose[:, :3]
+            abs_VO_pose[:, -1:] += rel_VO_pose[:3, -1:]
 
             print('对齐到雷达坐标系的绝对位姿')
             print(abs_VO_pose)
 
             if args.output_dir is not None:
                 predictions_array[j] = final_poses
-                abs_VO_poses[j] = abs_VO_pose.reshape(-1, 12)[0]
-
-            # ATE, RE = compute_pose_error(sample['poses'], final_poses)
-            # errors[j] = ATE, RE
-
-            #
-            # '''
-            # LO部分
-            # 以VO的结果作为预估，并计算和真值的差值
-            # '''
-            # pointclouds=sample['pointclouds']
-            # from VOLO import LO
-            # #pointcluds是可以直接处理的
-            # for i, pc in enumerate(pointclouds):
-            #     if i == len(pointclouds)//2:
-            #
-            #         tgt_pc =pointclouds[i]
-            #
-            # optimized_transform_matrices=[]
-            #
-            # timeCostLO=0
-            # startTimeLO=time.time()
-            # totalIterations=0
-            # for i,pc in enumerate(pointclouds):
-            #     pose_proposal=np.identity(4)
-            #     pose_proposal[:3,:]=transform_matrices[i]
-            #     print('======pose proposal for LO=====')
-            #     print(pose_proposal)
-            #
-            #     T,distacnces,iterations=LO(pc,tgt_pc,init_pose=pose_proposal, max_iterations=50, tolerance=0.001,LO='icp')
-            #     optimized_transform_matrices.append(T)
-            #     totalIterations+=iterations
-            #     print('iterations:\n')
-            #     print(iterations)
-            # timeCostLO=time.time()-startTimeLO
-            # optimized_transform_matrices=np.asarray(optimized_transform_matrices)
-            #
-            # print('*****LO result: time_cost {:.3} s'.format(timeCostLO/(len(pointclouds)-1))+' average iterations: {}'
-            #       .format(totalIterations/(len(pointclouds)-1)))
-            # # print(optimized_transform_matrices)
-            #
-            #
-            # #TODO 打通VO-LO pipeline: 需要将转换矩阵格式对齐； 评估VO的预估对LO的增益：效率上和精度上； 评估过程可视化
-            # #TODO 利用数据集有对应的图像，点云和位姿真值的数据集（Kitti的odomerty）
-            #
-            # inv_optimized_rot_matrices = np.linalg.inv(optimized_transform_matrices[:,:3,:3])
-            # inv_optimized_tr_vectors = -inv_optimized_rot_matrices @ optimized_transform_matrices[:,:3,-1:]
-            # inv_optimized_transform_matrices = np.concatenate([inv_optimized_rot_matrices, inv_optimized_tr_vectors], axis=-1)
-            #
-            # first_inv_optimized_transform = inv_optimized_transform_matrices[0]
-            # final_optimized_poses = first_inv_optimized_transform[:,:3] @ optimized_transform_matrices[:,:3,:]
-            # final_optimized_poses[:,:,-1:] += first_inv_optimized_transform[:,-1:]
-            # # print('first')
-            # # print(first_inv_optimized_transform)
-            # print('poses')
-            # print(final_optimized_poses)
-            #
-            # if args.output_dir is not None:
-            #     predictions_array[j] = final_poses
-            #
-            # optimized_ATE, optimized_RE = compute_pose_error(sample['poses'], final_optimized_poses)
-            # optimized_errors[j] = optimized_ATE, optimized_RE
-            #
-            # print('==============\n===============\n')
+                abs_VO_poses[j] = abs_VO_pose.reshape(12)
 
         mean_errors = errors.mean(0)
         std_errors = errors.std(0)
@@ -416,8 +315,8 @@ def main():
         print("std \t {:10.4f}, {:10.4f}".format(*optimized_std_errors))
 
         if args.output_dir is not None:
-            #np.save(output_dir / 'predictions.npy', predictions_array)
-            np.savetxt(output_dir / 'abs_VO_poses.txt', abs_VO_poses)
+            np.save(save_dir / 'predictions.npy', predictions_array)
+            np.savetxt(save_dir / 'abs_VO_poses.txt', abs_VO_poses)
 
 
 def compute_pose_error(gt, pred):
@@ -437,6 +336,17 @@ def compute_pose_error(gt, pred):
         RE += np.arctan2(s, c)
 
     return ATE / snippet_length, RE / snippet_length
+
+
+def GramSchmidtHelper(transformation):
+    a1 = Matrix(transformation[0, :3])
+    a2 = Matrix(transformation[1, :3])
+    a3 = Matrix(transformation[2, :3])
+    so3 = [a1, a2, a3]
+    O = GramSchmidt(so3, True)
+    O = np.array(O)
+    transformation[:3, :3] = O[:3, :3].reshape(3, 3)
+    return transformation
 
 
 if __name__ == '__main__':
